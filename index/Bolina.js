842b022b61fba95f958e4dd5f459300613947047
pearlbolina <pearlanolynbolina@gmail.com>
2024-05-21 06:45:25 +0800

    Add files via upload

---
+++ index/Bolina.js
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+    <head> 
+        <meta charset="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="veiwport" content="width=device-width, initial-scale=1.0"/>
+        <link
+        href="https://cdn.jsdelivr.net/npm/remixicon@4.2.0/fonts/remixicon.css"
+        rel="stylesheet"
+    />"
+    <link rel="stylesheet" href="style.css" />
+        <title>
+            Web design Mastery 
+        </title>
+    </head>
+    <body>
+        <nav>
+            <div class="nav_content">
+                <div class="logo"><a href="#">Pearl</a></div>
+                <label for="check" class="checkbox">
+                    <i class="ri-menu-line"></i>
+                </label>
+                <input type="checkbox" name="check" id="check">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Hobbies</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+        <section class="section">
+            <div class="section_container">
+                <div class="content">
+                <P class="subtitle">HELLO</p>
+                    <h1 class="title">
+                        I'm <span>Pearl Bolina<br>a</span> Grade-9 student from RTPM-DSHS. </h1>
+                        <p class="description">
+                            I'm 15 years old and turning 16 this year. I love to act,dance,sing and play sports. My hobbies are playing with my phone,eat and study. 
+                        
+                                links of the activities:
+                                https://drive.google.com/file/d/16qaxyx8Y8rOMUGuXeKMpFDx6YgAuRBjA/view?usp=drivesdk
+                                https://drive.google.com/file/d/14HaMTHn5uxVWlNqu0VAqchl48tK6SyjN/view?usp=drivesdk
+                                https://drive.google.com/file/d/13MCoGBipG0B9r0Y3aHkk2l62ZJ11E3Sk/view?usp=drivesdk
+                                https://drive.google.com/file/d/1JSpzKRqr63WhBXTBSzf4ay82HW6ngiVA/view?usp=drivesdk
+                                https://drive.google.com/file/d/1BFFmRuCxKyhwdNKHWZV_HAi0KM-G2mGo/view?usp=drivesdk
+                                https://drive.google.com/file/d/1VbYT7p2E8NJOLBhHETQ9vHYgWdzpU6-0/view?usp=drivesdk
+                            </p> 
+                        <div class="action_btns">
+                            <button class="Let's be friends!">Let's be Friends!</button>
+                            <button class="portfolio">portfolio!</button>
+                        </div>
+                        </div>
+                        <div class="image">
+                            <img src="img/img/441655791_459789463121671_638401675915987638_n.jpg" alt="profile"/>
+                            
+                           
+                        </div>
+                    </section>
+    </body>
+</html>
